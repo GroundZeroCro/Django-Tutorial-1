85c373bde06ee6961d6ff258f4dc74172d1009cd
Mario Dujić <liberasoftware@gmail.com>
2020-02-22 17:34:22 +0100

    Part 6

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # Passing function, not executing with ()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
--- blog/views.py
+++ blog/views.py
@@ -2,27 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Zero',
-        'title': 'Devv',
-        'content': 'Android code',
-        'date_posted': 'August 27, 2020.'
-    },
-    {
-        'author': 'Ground',
-        'title': 'Dev',
-        'content': 'Angular code',
-        'date_posted': 'August 21, 2020.'
-    },
-]
-
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
