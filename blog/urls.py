b9e1a4ee8c2c34492c2cdb19cfaf3a991e018c5f
Mario Dujić <liberasoftware@gmail.com>
2020-02-22 07:33:16 +0100

    end of Part 2

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url('', views.home, name='blog-home')
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
