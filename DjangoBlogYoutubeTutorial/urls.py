62d18ac7bd1d097999006b4715c8912074766a99
Mario Dujić <liberasoftware@gmail.com>
2020-03-10 19:17:04 +0100

    Part 8 25:00

--- DjangoBlogYoutubeTutorial/urls.py
+++ DjangoBlogYoutubeTutorial/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
@@ -25,3 +28,6 @@ urlpatterns = [
     url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url('profile/', user_views.profile, name='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
